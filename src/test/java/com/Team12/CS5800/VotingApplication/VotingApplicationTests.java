fed0a16603d406c5e06fbcbc28b87273ae174670
Heather Kemp <heather-kemp@uiowa.edu>
2018-03-14 22:40:11 -0500

    Add rest of the page tests

--- src/test/java/com/Team12/CS5800/VotingApplication/VotingApplicationTests.java
+++ src/test/java/com/Team12/CS5800/VotingApplication/VotingApplicationTests.java
@@ -30,5 +30,33 @@ public class VotingApplicationTests {
 			e.printStackTrace();
 		}
 	}
+	
+	@Test
+	public void indexLoads() {
+		try {
+			HttpResponse<String> response = Unirest.get("http://localhost:8080/")
+					  .header("Cache-Control", "no-cache")
+					  .header("Postman-Token", "4a6ab756-d2d7-40d8-95f0-888c3489ce70")
+					  .asString();
+			org.junit.Assert.assertEquals(response.getStatus(), 200);
+		} catch (UnirestException e) {
+			org.junit.Assert.fail("Failed to execute the Unirest");
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void registerLoads() {
+		try {
+			HttpResponse<String> response = Unirest.get("http://localhost:8080/register")
+					  .header("Cache-Control", "no-cache")
+					  .header("Postman-Token", "ce3a042b-ddba-44d4-b019-851aa34dd035")
+					  .asString();
+			org.junit.Assert.assertEquals(response.getStatus(), 200);
+		} catch (UnirestException e) {
+			org.junit.Assert.fail("Failed to execute the Unirest");
+			e.printStackTrace();
+		}
+	}
 
 }
